8c77e18d509405933881c6d526c7de923413c041
Alyona Klekovkina <like.the.sunshine.17@gmail.com>
2020-09-29 14:06:51 +0300

    Stage #3: Endless game

--- rps/game.py
+++ rps/game.py
@@ -1,13 +1,21 @@
 import random
 
 # Write your code here
-users_option = input()
-options = ['scissors', 'rock', 'paper']
-computer_option = random.choice(options)
+while True:
+    users_option = input()
+    computer_option = random.choice(['scissors', 'rock', 'paper'])
+    win = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
 
-if users_option == computer_option:
-    print("There is a draw ({})".format(users_option))
-elif (users_option == 'rock' and computer_option == 'scissors') or (users_option == 'paper' and computer_option == 'rock') or (users_option == 'scissors' and computer_option == 'paper'):
-    print("Well done. The computer chose {} and failed".format(computer_option))
-else:
-    print("Sorry, but the computer chose {}".format(computer_option))
+    if users_option == '!exit':
+        print("Bye!")
+        break
+    elif users_option not in win:
+        print("Invalid input")
+    elif users_option == computer_option:
+        print("There is a draw ({})".format(users_option))
+    elif win[users_option] == computer_option:
+        print("Well done. The computer chose {} and failed".format(computer_option))
+    elif win[users_option] != computer_option:
+        print("Sorry, but the computer chose {}".format(computer_option))
+
+
